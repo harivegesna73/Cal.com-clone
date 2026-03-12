51a3fe9290299cbec1c0ad1a7eae79d42ab99fbf
hari ram <hariram.v@techsophy.com>
2026-03-12 19:19:44 +0530

    fix: force IPv4 DNS resolution for nodemailer

--- server/server.js
+++ server/server.js
@@ -1,4 +1,8 @@
 require("dotenv").config({ path: require("path").join(__dirname, ".env") });
+
+const dns = require("dns");
+dns.setDefaultResultOrder("ipv4first");
+
 const express = require("express");
 const cors = require("cors");
 const { Pool } = require("pg");
